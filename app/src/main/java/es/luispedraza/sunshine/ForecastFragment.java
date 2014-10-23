6392c86fc4e570d0568de4047151e581b0722b96
Luis Pedraza <luispedraza@gmail.com>
2014-10-23 11:33:53 +0200

    added a separate class for async background retrieval of data from server

---
+++ app/src/main/java/es/luispedraza/sunshine/ForecastFragment.java
@@ -0,0 +1,121 @@
+package es.luispedraza.sunshine;
+
+import android.app.Fragment;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ForecastFragment extends Fragment {
+
+    private ArrayAdapter<String> mForecastAdapter;
+
+    public ForecastFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecastArray = {
+                "Today - Sunny - 34/19",
+                "Tomorrow - Foggy - 29/18",
+                "Today - Sunny - 34/19",
+                "Tomorrow - Foggy - 29/18",
+                "Today - Sunny - 34/19",
+                "Tomorrow - Foggy - 29/18",
+                "Today - Sunny - 34/19",
+                "Tomorrow - Foggy - 29/18",
+                "Today - Sunny - 34/19",
+                "Tomorrow - Foggy - 29/18"
+        };
+        ArrayList<String> weekForecast = new ArrayList<String>(
+                Arrays.asList(forecastArray)
+        );
+
+        mForecastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast);
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
+    }
+
+    private class FetchWeatherTask extends AsyncTask {
+
+        private final String LOG_TAG = this.getClass().getSimpleName();  // for logs
+        @Override
+        protected String doInBackground(Object[] objects) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // will contain the JSON response (string)
+            String forecastJsonStr = null;
+
+            try {
+                // URL for OpenWeatherMap query
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=28002&mode=json&units=metric&cnt=7");
+                // create the request and open connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // read input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n"); // adding newline for easier debugging
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+
+                forecastJsonStr = buffer.toString();
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error", e);
+                forecastJsonStr = null;
+
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return forecastJsonStr;
+        }
+    }
+}
--- app/src/main/java/es/luispedraza/sunshine/MainActivity.java
+++ app/src/main/java/es/luispedraza/sunshine/MainActivity.java
@@ -1,20 +1,9 @@
 package es.luispedraza.sunshine;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 
 public class MainActivity extends Activity {
@@ -25,7 +14,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -48,50 +37,5 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        private ArrayAdapter<String> mForecastAdapter;
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-
-
-            String[] forecastArray = {
-                    "Today - Sunny - 34/19",
-                    "Tomorrow - Foggy - 29/18",
-                    "Today - Sunny - 34/19",
-                    "Tomorrow - Foggy - 29/18",
-                    "Today - Sunny - 34/19",
-                    "Tomorrow - Foggy - 29/18",
-                    "Today - Sunny - 34/19",
-                    "Tomorrow - Foggy - 29/18",
-                    "Today - Sunny - 34/19",
-                    "Tomorrow - Foggy - 29/18"
-            };
-            ArrayList<String> weekForecast = new ArrayList<String>(
-                    Arrays.asList(forecastArray)
-            );
-
-            mForecastAdapter = new ArrayAdapter<String>(
-                    getActivity(),
-                    R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textview,
-                    weekForecast);
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(mForecastAdapter);
-
-            return rootView;
-        }
     }
 }
